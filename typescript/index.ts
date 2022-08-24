38f2df17206426ec26aba6cc06f1a53106ece226
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-24 23:38:14 +0700

    fix: Update inverse test

--- typescript/index.ts
+++ typescript/index.ts
@@ -2,3 +2,6 @@ import bubbleSort from "./bubble-sort";
 
 const bubbleArr = bubbleSort([1, 3, 2, 9, 4, 7, 6, 10, 5]);
 console.log(bubbleArr);
+
+const bubbleArrInverse = bubbleSort([1, 3, 2, 9, 4, 7, 6, 10, 5], true);
+console.log(bubbleArrInverse);
