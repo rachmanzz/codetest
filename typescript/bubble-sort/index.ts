17f97dcb52f00f4f110c0b161db304c304d1894f
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-24 23:36:25 +0700

    feat: Inverse result

--- typescript/bubble-sort/index.ts
+++ typescript/bubble-sort/index.ts
@@ -1,8 +1,9 @@
-const bubbleSort = (arr: number[]) => {
+const bubbleSort = (arr: number[], inverse: boolean = false): number[] => {
     const size = arr.length;
     for (let i = 0; i < size; i++) {
         for (let j = i+1; j < size; j++) {
-            if (arr[j] < arr[i]) {
+            const check: boolean = inverse ? arr[j] > arr[i] : arr[j] < arr[i];
+            if (check) {
                 const current = arr[i];
                 arr[i] = arr[j];
                 arr[j] = current;
