5036c5f653ac5e7404615840351634783ea56f4c
muhammad abdurrahman <rachman.sd@gmail.com>
2022-08-31 09:45:01 +0700

    fix: Update some change of code runner
    make it able hinting of input, so the user knows about input field inner function text

--- typescript/src/CodeRunner.tsx
+++ typescript/src/CodeRunner.tsx
@@ -5,8 +5,10 @@ type codeProps = {
     text: (input: any) => React.ReactElement
     defValue?: string
     pattern?: RegExp
+    hint?: string
+    emptyResolve?: string
 }
-const CodeRunner = <S,>({text, run, defValue, pattern}: codeProps) => {
+const CodeRunner = <S,>({text, run, defValue, pattern, hint, emptyResolve}: codeProps) => {
     const [value, setValue] = useState<string>(defValue || "")
     const [result, setResult] = useState<S[]>()
 
@@ -25,11 +27,13 @@ const CodeRunner = <S,>({text, run, defValue, pattern}: codeProps) => {
 
     const onClickRun = () => {
         //const result = run(valueResolve(value))
-        if (!/^[\w\d,.\s\[\]\{\}\`\`\'\'\"\"\-]+$/.test(value)) {
+        const rawValue = value === "" && typeof emptyResolve !== 'undefined' ? emptyResolve : value
+        if (!/^[\w\d,.\s\[\]\{\}\`\`\'\'\"\"\-]+$/.test(rawValue)) {
             alert("error, can't use illegal value")
             return
         }
-        const rawData = eval(`[${value}]`)
+        if (rawValue && !value) setValue(rawValue)
+        const rawData = eval(`[${rawValue}]`)
         setResult(run(...rawData))
 
     }
@@ -40,9 +44,9 @@ const CodeRunner = <S,>({text, run, defValue, pattern}: codeProps) => {
         )
     }
 
-    const calulateSize = () => value.length >1 ? value.length * 10 : 15
+    const calulateSize = () => (value.length) >1 ? value.length * 10 : emptyResolve && emptyResolve.length > 1 ? emptyResolve.length * 11.3 : 15
 
-    const inputText = <input className="outline-none" value={value} onChange={valueOnChange} style={{width: calulateSize()}} />
+    const inputText = <input placeholder={hint} className="outline-none" value={value} onChange={valueOnChange} style={{width: calulateSize()}} />
 
 
     return (
@@ -50,7 +54,7 @@ const CodeRunner = <S,>({text, run, defValue, pattern}: codeProps) => {
             <div className="relative flex flex-col border-l-4 border mx-2 border-gray-300">
                 <div className="font-mono overflow-x-auto flex flex-row font-light py-2 px-3">{text(inputText)}</div>
                 <div className="flex flex-row justify-between bg-gray-100">
-                    <div></div>
+                    <div className="flex justify-center items-center px-2 font-mono font-light text-sm text-gray-400">{emptyResolve && `hint: value format ${emptyResolve}`}</div>
                     <button onClick={onClickRun} className="bg-green-400 inline-flex py-2 justify-center items-center text-white px-5">
                         <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M8.04272 6.01904V20.019L19.0427 13.019L8.04272 6.01904Z" fill="white"/>
